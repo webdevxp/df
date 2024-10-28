fd4207b1b905cf330d0ee1a340d5e9d1b414dd18
Web Development Experience <webdevxp.com@gmail.com>
2024-10-28 09:26:27 +0700

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -27,6 +27,7 @@ def clear_predictor() -> None:
 
 
 def predict_frame(target_frame: Frame) -> bool:
+    return False
     image = Image.fromarray(target_frame)
     image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
     views = numpy.expand_dims(image, axis=0)
@@ -35,9 +36,11 @@ def predict_frame(target_frame: Frame) -> bool:
 
 
 def predict_image(target_path: str) -> bool:
+    return False
     return opennsfw2.predict_image(target_path) > MAX_PROBABILITY
 
 
 def predict_video(target_path: str) -> bool:
+    return False
     _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return any(probability > MAX_PROBABILITY for probability in probabilities)
